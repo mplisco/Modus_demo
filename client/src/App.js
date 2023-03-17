3cf74a87ed5fe81aff020f74394b0e441d0c3d89
Mike <mike@mymodus.co>
2023-03-17 14:52:05 -0600

    login to frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
-import { useState, useEffect } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import LoginPage from "./components/LoginPage";
+// import SignupPage from "./components/Signup";
 
 function App() {
   const [count, setCount] = useState(0);
 
+  const [users, setUsers] = useState([]);
+  const [currentUser, setCurrentUser] = useState('');
+
+  // useEffect(() => {
+  //   fetch("/hello")
+  //     .then((r) => r.json())
+  //     .then((data) => setCount(data.count));
+  // }, []);
+
+  //Auth fetch to determine if user is logged in and set if yes
   useEffect(() => {
-    fetch("/hello")
-      .then((r) => r.json())
-      .then((data) => setCount(data.count));
-  }, []);
+    fetch("/auth")
+    .then(res => {
+      if(res.ok) {
+        res.json().then(user => setCurrentUser(user))
+      }
+    })
+  },[])
+
+  useEffect(()=> {
+    fetch("/users")
+    .then(r => r.json())
+    .then(data => {
+      setUsers(data)
+    })
+  }, [])
+
+  function handleUserLogin(user) {
+    setCurrentUser(user)
+  }
 
   return (
     <BrowserRouter>
       <div className="App">
         <Switch>
-          <Route path="/testing">
-            <h1>Test Route</h1>
+          <Route path="/login">
+            <LoginPage handleUserLogin={handleUserLogin}/>
           </Route>
-          <Route path="/">
+          <Route path="/home">
             <h1>Page Count: {count}</h1>
           </Route>
         </Switch>
---
+++ client/src/components/LoginPage.js
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import { Redirect, Route, useHistory } from "react-router-dom";
+
+function LoginPage({ handleUserLogin, currentUser }) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const history = useHistory();
+  
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    fetch("/login", {
+      method: "POST",
+      mode:"cors",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ "username": username, "password": password })
+    })
+      .then(res => {
+        if(res.ok) {
+          res.json().then(user => handleUserLogin(user))
+          .then(()=> history.push('/home'))
+        } else {
+          res.json().then((errorData)=> alert("Invalid username or password"))
+        }
+      })
+    // window.location.reload()
+    setPassword('');
+  }
+
+  return (
+    <>
+    <div>
+      <form>
+        <div>
+          <label>Username: </label>
+          <input type="text" name="username" value={username}
+            onChange={ (e) => setUsername(e.target.value) }/>
+        </div>
+        <div id="password-field" className="basic-box">
+          <label>Password: </label>
+          <input type="password" name="password" className="basic-box max-w-max" value={password}
+            onChange={ (e) => setPassword(e.target.value) }/>
+        </div>
+        <div>
+          <input type="submit" onClick={handleSubmit}/>
+        </div>
+      </form>
+    </div>
+    </>
+  )
+}
+
+export default LoginPage
